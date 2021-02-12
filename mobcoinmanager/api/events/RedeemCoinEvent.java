58aa644264dbcec68b2f4f8463cb24c4b530ed31
H1DD3NxN1NJA <kylehawkwojo@gmail.com>
2021-02-11 19:46:25 -0500

    Create RedeemCoinEvent.java

---
+++ mobcoinmanager/api/events/RedeemCoinEvent.java
@@ -0,0 +1,33 @@
+package me.h1dd3nxn1nja.mobcoinmanager.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class RedeemCoinEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+	private Player player;
+	private int amount;
+
+	public RedeemCoinEvent(Player player, int amount) {
+	    this.player = player;
+	    this.amount = amount;
+	  }
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
